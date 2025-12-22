d98a808060a71017705ec89c7b6dc933894b11d4
Matthew Belcher <mattb@selinicapital.com>
2025-12-22 15:36:24 +0000

    [ena xdp] account for rhel backport and relax version

--- kernel/linux/ena/kcompat.h
+++ kernel/linux/ena/kcompat.h
@@ -1091,11 +1091,13 @@ static inline void skb_metadata_set(struct sk_buff *skb, u8 meta_len) {}
 #endif /* ENA_PAGE_POOL_SUPPORT */
 
 /* XDP metadata ops for bpf_xdp_metadata_rx_timestamp() kfunc support
- * Introduced in kernel 6.3
+ * Introduced in kernel 6.3, but backported to RHEL 9 (5.14.0-xxx)
  */
-#if defined(ENA_XDP_SUPPORT) && LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
+#if defined(ENA_XDP_SUPPORT) && \
+    (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0) || \
+     (defined(RHEL_RELEASE_CODE) && RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 0)))
 #define ENA_HAVE_XDP_METADATA_OPS
-#endif /* ENA_XDP_SUPPORT && KERNEL >= 6.3 */
+#endif /* ENA_XDP_SUPPORT && (KERNEL >= 6.3 || RHEL >= 9.0) */
 
 #ifndef DEFINE_SHOW_ATTRIBUTE
 #define DEFINE_SHOW_ATTRIBUTE(__name)					\
